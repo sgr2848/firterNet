e7cb60c1825b79d6f7ef06138c9882826107e617
theomgxz <sg28r8@gmail.com>
2020-02-08 15:54:41 -0500

    creation of data works

--- create_data.py
+++ create_data.py
@@ -4,11 +4,15 @@ import requests, lxml
 from selenium.webdriver import Firefox
 import re
 import random
-# import concurrent.futures
-import threading,os,time
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+import os,time
+#import threading
 # import multiprocessing
 from typing import List
 import importlib
+import numpy as np
+import pickle
 
 def check_reddit_class(someString):
     if re.match('class=" thing id-t3_f[\d|a-z]{5}', someString):
@@ -19,18 +23,27 @@ def check_reddit_class(someString):
         # print(".",end="---")
         return False
 
-def get_comment_data(url):
+def get_comment_data(url) :
 
     res = requests.get(url,  headers={'User-agent': 'No a bot I swear'})
     if res.status_code == 200:
         b_soup = bs.BeautifulSoup(res.text, features="lxml")
         # a = b_soup.find_all('div', {"id": "thing_t1_cwds4nk"})
         a = re.findall('form-t1_[\d|a-z]{10}', str(b_soup))
-        print(f"{url[20:]} has {len(a)} comments")
-     
+        # print(f"{url[20:]} has {len(a)} comments")
+        word_matrix = []
+        for i in a:
+            current_comment = b_soup.select('form', {'id': i})
+            for para in current_comment:
+                for i in para.findAll('p'):
+                    # print(".",end="|")
+                    word_matrix.append(i.text)
+        return word_matrix
+
     else:
         print(res.text)
-        print(f"colund't get in for some unknown reason {res.status_code}")
+        print(f"couldn't get in for some unknown reason : {res.status_code} ")
+        return ["couldn't grab shit"]
 def make_data():
     """
         the main function I guess goes to reddit gets the top 25 post and collects the first page comment of the the page and give you a pickled file....
@@ -38,7 +51,7 @@ def make_data():
     
     op = Options()
     op.headless = True
-    brow = Firefox(options = op)
+    brow = Firefox(executable_path = 'geckodriver.exe',options = op)
 
     brow.set_page_load_timeout('10')
     brow.get("https://old.reddit.com/")  # think there might be a function that is much better solution than sleeping but oh welllllll
@@ -58,20 +71,32 @@ def make_data():
     for i in feed_list:
         a = re.findall('data-permalink="/r/[\d|a-z|A-Z|-|_]+/comments/[\d|a-z|A-Z|-|_]+/[\d|a-z|A-Z|-|_]+/', i)
         content_list.append(f"http://old.reddit.com{a[0][16:]}")
-
     brow.quit()    
     threads = [] 
     time_a = time.perf_counter()
-    for i in content_list:
-        thread = threading.Thread(target=get_comment_data,args=[i])
-        thread.start()
-        threads.append(thread)
-    for thread in threads:
-         thread.join()
+    # for i in content_list:
+    #     thread = threading.Thread(target=get_comment_data,args=[i])
+    #     thread.start()
+    #     threads.append(thread)
+    # for thread in threads:
+    #      thread.join()
     #syncro took 36 secs || threading is more efficient way
     # for i in content_list:
     #     get_comment_data(i)
-    print(f"{time.perf_counter()-time_a} secs || {len(content_list)} ")
+    comment_list = []
+    time_list = []
+    with ThreadPoolExecutor(max_workers=10) as exe:
+        futures = [exe.submit(get_comment_data, url) for url in content_list]
+        for future in as_completed(futures):
+            print("***DONZOO***", end="_|_")
+            print(time.perf_counter()-time_a)
+            time_list.append(time.perf_counter()-time_a)
+            comment_list.append(future.result())
+
+    with open("data.txt", "a", encoding="utf-8") as data_file:
+        for i in comment_list:
+            data_file.write(str(i))
+    print(f"{(time.perf_counter()-time_a)/60} mins || {len (content_list)}")
 if __name__ == "__main__":
     sel_as = importlib.util.find_spec("selenium")
     bs4_as = importlib.util.find_spec("bs4")
@@ -80,6 +105,7 @@ if __name__ == "__main__":
         print(sel_as)
         print(bs4_as)
         print(lxml_as)
+        # get_comment_data("https://old.reddit.com/r/worldnews/comments/f0v18s/buried_in_trumps_peace_plan_a_proposal_that_could/")
         make_data()
     else:
         os.system("python -m pip install -r requirements.txt")
---
+++ filterNet.py
@@ -0,0 +1,17 @@
+import tensorflow as tf 
+import numpy as np
+import pandas as pd
+import tensorflow import keras
+from tensorflow.keras import Sequential
+from  tensorflow.keras.layers import Dense, Flatten, LSTM 
+
+def model(feed_text: List):
+    model = Sequential()
+    model.add()
+    model.add()
+    model.add()
+    model.compile()
+    model.fit()
+    m
+def analyse(some_text: str,model) -> int:
+    pass    
